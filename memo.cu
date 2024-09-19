bb6abc327d8b1196578575023864207958f8438e
tp24002 <tp24002-7786@gs4e.chubu.ac.jp>
2024-09-19 11:51:56 +0900

    copyHostToDevice error

--- memo.cu
+++ memo.cu
@@ -1,77 +1,38 @@
-void MemoryBefAftToDevice(BefAft *h, BefAft *d, Range ran) {
-    cudaMemcpy(&h, &d, sizeof(BefAft), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h->sa, &d->sa, sizeof(SigArr), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h->ta, &d->ta, sizeof(TauArr), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h->va, &d->va, sizeof(VelArr), cudaMemcpyDeviceToHost);
-    for(int i = 0; i < ran.sr.Txx.x; i++) {
-        for(int j = 0; j < ran.sr.Txx.y; j++){
-            cudaMemcpy(&h->sa.Txx, &d->sa.Txx, sizeof(double) * ran.sr.Txx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Txxx, &d->sa.Txxx, sizeof(double) * ran.sr.Txx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Txxy, &d->sa.Txxy, sizeof(double) * ran.sr.Txx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Txxz, &d->sa.Txxz, sizeof(double) * ran.sr.Txx.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.sr.Tyy.x; i++) {
-        for(int j = 0; j < ran.sr.Tyy.y; j++){
-            cudaMemcpy(&h->sa.Tyy[i][j], &d->sa.Tyy[i][j], sizeof(double) * ran.sr.Tyy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tyyx[i][j], &d->sa.Tyyx[i][j], sizeof(double) * ran.sr.Tyy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tyyy[i][j], &d->sa.Tyyy[i][j], sizeof(double) * ran.sr.Tyy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tyyz[i][j], &d->sa.Tyyz[i][j], sizeof(double) * ran.sr.Tyy.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.sr.Tzz.x; i++) {
-        for(int j = 0; j < ran.sr.Tzz.y; j++){
-            cudaMemcpy(&h->sa.Tzz[i][j], &d->sa.Tzz[i][j], sizeof(double) * ran.sr.Tzz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tzzx[i][j], &d->sa.Tzzx[i][j], sizeof(double) * ran.sr.Tzz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tzzy[i][j], &d->sa.Tzzy[i][j], sizeof(double) * ran.sr.Tzz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->sa.Tzzz[i][j], &d->sa.Tzzz[i][j], sizeof(double) * ran.sr.Tzz.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    //////
-    for(int i = 0; i < ran.tr.Txy.x; i++) {
-        for(int j = 0; j < ran.tr.Txy.y; j++){
-            cudaMemcpy(&h->ta.Txy[i][j], &d->ta.Txy[i][j], sizeof(double) * ran.tr.Txy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Txyx[i][j], &d->ta.Txyx[i][j], sizeof(double) * ran.tr.Txy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Txyy[i][j], &d->ta.Txyy[i][j], sizeof(double) * ran.tr.Txy.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.tr.Tyz.x; i++) {
-        for(int j = 0; j < ran.tr.Tyz.y; j++){
-            cudaMemcpy(&h->ta.Tyz[i][j], &d->ta.Tyz[i][j], sizeof(double) * ran.tr.Tyz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Tyzy[i][j], &d->ta.Tyzy[i][j], sizeof(double) * ran.tr.Tyz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Tyzz[i][j], &d->ta.Tyzz[i][j], sizeof(double) * ran.tr.Tyz.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.tr.Tzx.x; i++) {
-        for(int j = 0; j < ran.tr.Tzx.y; j++){
-            cudaMemcpy(&h->ta.Tzx[i][j], &d->ta.Tzx[i][j], sizeof(double) * ran.tr.Tzx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Tzxz[i][j], &d->ta.Tzxz[i][j], sizeof(double) * ran.tr.Tzx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->ta.Tzxx[i][j], &d->ta.Tzxx[i][j], sizeof(double) * ran.tr.Tzx.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    ///////
-    for(int i = 0; i < ran.vr.Vx.x; i++) {
-        for(int j = 0; j < ran.vr.Vx.y; j++){
-            cudaMemcpy(&h->va.Vx, &d->va.Vx, sizeof(double) * ran.vr.Vx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vxx, &d->va.Vxx, sizeof(double) * ran.vr.Vx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vxy, &d->va.Vxy, sizeof(double) * ran.vr.Vx.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vxz, &d->va.Vxz, sizeof(double) * ran.vr.Vx.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.vr.Vy.x; i++) {
-        for(int j = 0; j < ran.vr.Vy.y; j++){
-            cudaMemcpy(&h->va.Vy, &d->va.Vy, sizeof(double) * ran.vr.Vy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vyx, &d->va.Vyx, sizeof(double) * ran.vr.Vy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vyy, &d->va.Vyy, sizeof(double) * ran.vr.Vy.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vyz, &d->va.Vyz, sizeof(double) * ran.vr.Vy.z, cudaMemcpyDeviceToHost);
-        }
-    }
-    for(int i = 0; i < ran.vr.Vz.x; i++) {
-        for(int j = 0; j < ran.vr.Vz.y; j++) {
-            cudaMemcpy(&h->va.Vz, &d->va.Vz, sizeof(double) * ran.vr.Vz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vzx, &d->va.Vzx, sizeof(double) * ran.vr.Vz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vzy, &d->va.Vzy, sizeof(double) * ran.vr.Vz.z, cudaMemcpyDeviceToHost);
-            cudaMemcpy(&h->va.Vzz, &d->va.Vzz, sizeof(double) * ran.vr.Vz.z, cudaMemcpyDeviceToHost);
-        }
-    }
-}
+// Copy function for SigArr
+    copy3DarrayHostToDevice(befaft_host->sa.Txx , &(befaft_device.sa.Txx) , ran.sr.Txx.x, ran.sr.Txx.y, ran.sr.Txx.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Txxx, &(befaft_device.sa.Txxx), ran.sr.Txx.x, ran.sr.Txx.y, ran.sr.Txx.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Txxy, &(befaft_device.sa.Txxy), ran.sr.Txx.x, ran.sr.Txx.y, ran.sr.Txx.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Txxz, &(befaft_device.sa.Txxz), ran.sr.Txx.x, ran.sr.Txx.y, ran.sr.Txx.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tyy , &(befaft_device.sa.Tyy) , ran.sr.Tyy.x, ran.sr.Tyy.y, ran.sr.Tyy.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tyyx, &(befaft_device.sa.Tyyx), ran.sr.Tyy.x, ran.sr.Tyy.y, ran.sr.Tyy.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tyyy, &(befaft_device.sa.Tyyy), ran.sr.Tyy.x, ran.sr.Tyy.y, ran.sr.Tyy.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tyyz, &(befaft_device.sa.Tyyz), ran.sr.Tyy.x, ran.sr.Tyy.y, ran.sr.Tyy.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tzz , &(befaft_device.sa.Tzz) , ran.sr.Tzz.x, ran.sr.Tzz.y, ran.sr.Tzz.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tzzx, &(befaft_device.sa.Tzzx), ran.sr.Tzz.x, ran.sr.Tzz.y, ran.sr.Tzz.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tzzy, &(befaft_device.sa.Tzzy), ran.sr.Tzz.x, ran.sr.Tzz.y, ran.sr.Tzz.z);
+    copy3DarrayHostToDevice(befaft_host->sa.Tzzz, &(befaft_device.sa.Tzzz), ran.sr.Tzz.x, ran.sr.Tzz.y, ran.sr.Tzz.z);
+
+    // Copy function for TauArr
+    copy3DarrayHostToDevice(befaft_host->ta.Txy , &(befaft_device.ta.Txy) , ran.tr.Txy.x, ran.tr.Txy.y, ran.tr.Txy.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Txyx, &(befaft_device.ta.Txyx), ran.tr.Txy.x, ran.tr.Txy.y, ran.tr.Txy.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Txyy, &(befaft_device.ta.Txyy), ran.tr.Txy.x, ran.tr.Txy.y, ran.tr.Txy.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tyz , &(befaft_device.ta.Tyz) , ran.tr.Tyz.x, ran.tr.Tyz.y, ran.tr.Tyz.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tyzy, &(befaft_device.ta.Tyzy), ran.tr.Tyz.x, ran.tr.Tyz.y, ran.tr.Tyz.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tyzz, &(befaft_device.ta.Tyzz), ran.tr.Tyz.x, ran.tr.Tyz.y, ran.tr.Tyz.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tzx , &(befaft_device.ta.Tzx) , ran.tr.Tzx.x, ran.tr.Tzx.y, ran.tr.Tzx.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tzxz, &(befaft_device.ta.Tzxz), ran.tr.Tzx.x, ran.tr.Tzx.y, ran.tr.Tzx.z);
+    copy3DarrayHostToDevice(befaft_host->ta.Tzxx, &(befaft_device.ta.Tzxx), ran.tr.Tzx.x, ran.tr.Tzx.y, ran.tr.Tzx.z);
+
+    // Copy function for VelArr
+    copy3DarrayHostToDevice(befaft_host->va.Vx , &(befaft_device.va.Vx) , ran.vr.Vx.x, ran.vr.Vx.y, ran.vr.Vx.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vxx, &(befaft_device.va.Vxx), ran.vr.Vx.x, ran.vr.Vx.y, ran.vr.Vx.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vxy, &(befaft_device.va.Vxy), ran.vr.Vx.x, ran.vr.Vx.y, ran.vr.Vx.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vxz, &(befaft_device.va.Vxz), ran.vr.Vx.x, ran.vr.Vx.y, ran.vr.Vx.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vy , &(befaft_device.va.Vy) , ran.vr.Vy.x, ran.vr.Vy.y, ran.vr.Vy.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vyx, &(befaft_device.va.Vyx), ran.vr.Vy.x, ran.vr.Vy.y, ran.vr.Vy.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vyy, &(befaft_device.va.Vyy), ran.vr.Vy.x, ran.vr.Vy.y, ran.vr.Vy.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vyz, &(befaft_device.va.Vyz), ran.vr.Vy.x, ran.vr.Vy.y, ran.vr.Vy.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vz , &(befaft_device.va.Vz) , ran.vr.Vz.x, ran.vr.Vz.y, ran.vr.Vz.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vzx, &(befaft_device.va.Vzx), ran.vr.Vz.x, ran.vr.Vz.y, ran.vr.Vz.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vzy, &(befaft_device.va.Vzy), ran.vr.Vz.x, ran.vr.Vz.y, ran.vr.Vz.z);
+    copy3DarrayHostToDevice(befaft_host->va.Vzz, &(befaft_device.va.Vzz), ran.vr.Vz.x, ran.vr.Vz.y, ran.vr.Vz.z);
